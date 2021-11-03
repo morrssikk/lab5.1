5ce0d54f06f4f9fd24a1d9de6f39d9cdd53e8357
morrssikk <91206206+morrssikk@users.noreply.github.com>
2021-11-03 19:15:37 +0200

    Добавьте файлы проекта.

---
+++ lab5.1/lab5.1.cpp
@@ -0,0 +1,20 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double a, const double b); // прототип h
+
+int main()
+{
+	double g, s;
+
+	cout << "g = "; cin >> g;
+	cout << "s = "; cin >> s;
+	double c = (h(g + 1, s) + pow(h(g, s + 1), 2)) / (1 + pow(h(pow(g, 2), pow(s, 2)), 3));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double a, const double b) // визначення h
+{
+	return a * a - b * b;
+}
